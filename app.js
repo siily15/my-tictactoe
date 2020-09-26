d47de3f41f559d51045e4cef0a2099853d5766d0
siily <arnokosk15@gmail.com>
2020-09-26 15:47:34 +0300

    html and css

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ function checkWin(board, player) {
 function gameOver(gameWon) {
     for (let index of winCombos[gameWon.index]) {
         document.getElementById(index).style.backgroundColor =
-            gameWon.player == huPlayer ? "blue" : "red";
+            gameWon.player == huPlayer ? "white" : "red";
     }
     for (var i = 0; i < cells.length; i++) {
         cells[i].removeEventListener('click', turnClick, false);
